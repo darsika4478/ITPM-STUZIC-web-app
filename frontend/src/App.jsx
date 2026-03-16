0589d1ecc7ab07af69746db4616f2500939bd8f7
Thisara Sandapium <156019603+TheAmazo@users.noreply.github.com>
2026-03-16 21:05:15 +0530

    Remove TasksPlanner page and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import AuthGuard from "./components/user-management/AuthGuard";
 import DashboardLayout from "./layout/DashboardLayout";
 import DashboardHome from "./pages/DashboardHome";
 import MyProfile from "./pages/MyProfile";
-import TasksPlanner from "./pages/TasksPlanner";
 import MoodRecommendationPage from "./features/mood_and_music_recommendation/pages/MoodRecommendationPage.jsx";
 
 export default function App() {
@@ -22,7 +21,6 @@ export default function App() {
         <Route path="/dashboard" element={<DashboardLayout />}>
           <Route index element={<DashboardHome />} />
           <Route path="profile" element={<MyProfile />} />
-          <Route path="tasks" element={<TasksPlanner />} />
         </Route>
       </Route>
 
